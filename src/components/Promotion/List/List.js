dfa2c1f04086b50ebc429c92476c935270d58cce
Alex Ricardo <alex.ricardo1999@hotmail.com>
2020-09-25 00:29:17 -0300

    custom hook of api request

--- src/components/Promotion/List/List.js
+++ src/components/Promotion/List/List.js
@@ -3,12 +3,20 @@ import PromotionCard from '../Card';
 
 import './List.scss';
 
-function PromotionList({ loading, promotions }) {
-    if (loading) {
+function PromotionList({ loading, promotions, error }) {
+    if (loading || promotions === null) {
         return (
-        <div className="promotion-loading">
-            <h2>Carregando...</h2>
-        </div>);
+            <div className="promotion-loading">
+                <h2>Carregando...</h2>
+            </div>);
+    }
+
+    if (promotions.length === 0) {
+        return <div> <h2>Nenhum resultado encontrado</h2> </div>
+    }
+
+    if (error) {
+        return <div><h2>Algo de errado não está certo.</h2></div>
     }
 
     return (
